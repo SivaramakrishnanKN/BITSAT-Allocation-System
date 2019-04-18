0071e2c66763069769e34080a9a98b226c5bb246
T1nkledeath <sivaram_sivaram99@yahoo.in>
2019-04-18 16:15:54 +0530

    Restructure

--- pages/PreferenceList.php
+++ pages/PreferenceList.php
@@ -3,27 +3,17 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
   <meta name="author" content="">
-  <title>Bootstrap 4 Tutorial</title>
   <link rel="stylesheet" href="css/bootstrap.min.css">
   <link rel="stylesheet" href="css/custom.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-  <script src="jquery-3.3.1.min.js"></script>
-  <script src="jquery.sortable.js"></script>
-  <script>
-  $('.list-group-sortable').sortable({
-    placeholderClass: 'list-group-item'
-  });
-  $('.sortable').sortable({
-    items: ':not(.disabled)'
-  });
-  </script>
+
 </head>
 
 <body>
 
-  <ul id="sortable1" class="list-group-sortable">
+  <ul id="sortable1" class="list-group connectedSortable">
 
   <li class="list-group-item active">Lion</li>
   <li class="list-group-item">Dog</li>
@@ -31,7 +21,7 @@
   <li class="list-group-item">Tiger</li>
 </ul>
 
-<ul id="sortable2" class="list-group-sortable">
+<ul id="sortable2" class="list-group connectedSortable">
   <li class="list-group-item active">Fish</li>
   <li class="list-group-item">Bird</li>
   <li class="list-group-item">Falcon</li>
